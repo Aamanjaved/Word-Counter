aa2225ea814997451245aeca5dc5576fe9e334b5
Aamanjaved <aamanad677@gmail.com>
2024-10-04 20:22:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let sentence = prompt("Type Word");
-let words = sentence.split(" ");
-let wordCount = 0;
+var sentence = prompt("Type Word");
+var words = sentence.split(" ");
+var wordCount = 0;
 
-for (let i = 0; i < words.length; i++) {
+for (var i = 0; i < words.length; i++) {
   if (words[i] !== "") {
     wordCount++;
   }
